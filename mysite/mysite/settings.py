904648a0eebf9fec0d95cceb0d785c5230796bba
jungmir <ham4051@gamil.com>
2020-12-30 15:58:53 +0900

    docker env setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-from . import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = key.Key().getKey()
+SECRET_KEY = "w9h6p_8-0pqt5cnqvu)l5hmzo!f$1bm_@&)i@1&t_ht5q87y7="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'admin',
+        'PASSWORD': '1234',
+        'HOST': 'mysql',
+        'PORT': '3307',
     }
 }
 
