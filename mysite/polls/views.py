02edc7a5c14e34a8d7b3597a2b6f5fe7307d3d14
Lee jaewon <jaewon1336@naver.com>
2021-01-06 18:32:14 +0900

    blog_tutorial

--- mysite/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def index(request):
-    context = {'user' : [{'id' : '1', 'name' : '홍길동'}, {'id' : '2', 'name' : '김철수'}]}
-    return render(request, 'polls/index.html', context)
